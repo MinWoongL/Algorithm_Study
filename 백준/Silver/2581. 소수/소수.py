3c198ff030424a123301d9a85e1b224f4dcb0cc5
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-01-22 20:10:00 +0900

    [Silver V] Title: 소수, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2581. 소수/소수.py
@@ -0,0 +1,34 @@
+# 소수 : M 이상 N이하 모든 소수들의 합과 최소값
+
+def find_prime_number(M,N):
+    li = []
+    p = True
+    for value in range(M,N+1):
+        if value == 1:
+            pass
+        elif value == 2 or value == 3:
+            li.append(value)
+        else:
+            sqrt_value = int(value ** 0.5)
+            for i in range(2, sqrt_value+1):
+                if value % i == 0:
+                    p = False
+                    break
+                else:
+                    p = True
+            if p:
+                li.append(value)
+
+    return li
+
+
+M = int(input())
+N = int(input())
+
+ans_li = find_prime_number(M, N)
+
+if len(ans_li) == 0:
+    print(-1)
+else:
+    print(sum(ans_li))
+    print(ans_li[0])
