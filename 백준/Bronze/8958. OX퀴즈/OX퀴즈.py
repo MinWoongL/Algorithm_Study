91abdae4e2eb734c69aa7e9b7ec2acfa37c5324f
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-01-24 01:18:37 +0900

    [Bronze II] Title: OX퀴즈, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,14 @@
+T = int(input())
+
+for i in range(T):
+    score = 0  # 총 점수
+    streak = 0  # 연속으로 정답일 때 점수 누적
+    text = str(input())
+    for v in range(len(text)):
+        if text[v] == "O":
+            streak += 1
+            score += streak
+        else:
+            streak = 0
+            
+    print(score)
