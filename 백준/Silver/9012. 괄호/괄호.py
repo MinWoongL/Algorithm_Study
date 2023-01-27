ed40d567649b9b5aeb999f3ed8e0d6cc4af52c10
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-01-27 15:47:11 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,29 @@
+# 9012_괄호_Parenthesis String_bracket
+
+import sys
+
+
+n = int(input())
+
+for i in range(n):
+    ps = list(map(str, sys.stdin.readline().strip()))
+    psb = True
+    while psb is True:
+        if len(ps) == 0:
+            print("YES")
+            break
+        elif ps[-1] == '(':
+            print("NO")
+            psb = False
+            break
+        else:
+            for i in range(len(ps)):
+                if ps[i] == ')':
+                    if ps[i-1] == '(':
+                        ps.pop(i-1)
+                        ps.pop(i-1)
+                        break
+                    else:
+                        print('NO')
+                        psb = False
+                        break
