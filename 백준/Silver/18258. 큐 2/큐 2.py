43faa920ce7d2995b417785470e4b06e111a1f3c
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-01-28 23:42:57 +0900

    [Silver IV] Title: 큐 2, Time: 2480 ms, Memory: 175068 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,40 @@
+# 18258_큐2_Queue2
+# deque : 보통 queue 는 FIFO 방식으로 이루어지지만 deque는 양방향 큐로써 append, pop 연산에 대해 우위를 가짐
+
+import sys
+from collections import deque
+
+n = int(input())
+dq = deque()
+
+
+for i in range(n):
+    msg = list(map(str, sys.stdin.readline().split()))
+
+    if msg[0] == 'push':
+        dq.append(msg[1])
+    elif msg[0] == 'front':
+        if dq:
+            print(dq[0])
+        else:
+            print(-1)
+    elif msg[0] == 'back':
+        if dq:
+            print(dq[-1])
+        else:
+            print(-1)
+    elif msg[0] == 'empty':
+        if dq:
+            print(0)
+        else:
+            print(1)
+    elif msg[0] == 'size':
+        print(len(dq))
+    else:
+        if dq:
+            print(dq.popleft())
+        else:
+            print(-1)
+
+
+
