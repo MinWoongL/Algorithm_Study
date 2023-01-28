c55c8308b97b45b116ab57c8ad6df5c67f7710d3
MinWoongLee <dlalsdnd2@gmail.com>
2023-01-29 02:44:54 +0900

    1110 더하기사이클 추가

--- Baekjoon/bronze_to_silver/1110_pluscycle.py
+++ Baekjoon/bronze_to_silver/1110_pluscycle.py
@@ -1,18 +1,26 @@
-#  더하기 사이클
-#  시간 초과 났음
+# 1110_더하기사이클
 
 num = int(input())
 
 changed = 0
 count = 0
-text = str(num)
 
-while changed != num:
-    c = str(int(text[-1]) + int(text[0]))
-    # changed = int(text[-1]+c[-1])
-    changed = int(''.join(text[-1], c[-1]))
-    text = str(changed)
-    count += 1
+if num < 10:  # 한자리 수 인경우 앞에 0을 붙여주는 작업 추가
+    text = '0'+str(num)
+else:
+    text = str(num)
+
+if num == 0:
+    print(1)
+else:
+    while changed != num:
+        c = str(int(text[-1]) + int(text[0]))
+        # changed = int(text[-1]+c[-1])
+        changed = str(text[-1]) + c[-1]  # int형으로 먼저 바꿔주면 '05' 가 5로 저장됨
+        text = changed
+        changed = int(changed)
+        count += 1
+
+    print(count)
 
 
-print(count)
