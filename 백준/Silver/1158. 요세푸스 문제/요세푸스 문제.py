1449d12bd4ce89640c71db9fc4c6986bae34c117
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-01-30 17:26:58 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,32 +1,22 @@
-# 11866_요세푸스문제0_Josephus 0
+T = list(map(int, input().split()))
 
-from collections import deque
+N = T[0]
+K = T[1]
 
-n, k = map(int, input().split())
+Num_list = []
+for i in range(1, N+1):
+    Num_list.append(i)
 
-dq = [] #  이 문제에서는 인덱스 넘버로 값을 제거할 필요가있음 deque는 불가능?
-jose_li = []
+num = 0
+sol = []
+for j in range(N):
+    num += K -1
+    if num >= len(Num_list):
+        num = num%len(Num_list)
+        
+    sol.append(str(Num_list.pop(num)))
 
-new_k = k-1
 
-for i in range(1,n+1):
-    dq.append(i)
+new =', '.join(sol)
 
-while len(dq) != 0:
-    if new_k >= len(dq):
-        # print('len(dq) = {}'.format(len(dq)))
-        new_k = new_k % len(dq)  # arr의 길이로 나눈 나머지만큼 다시 앞부터 계산
-        # print(new_k)
-    jose_li.append(dq[new_k])
-    dq.pop(new_k)
-    # print(dq)
-    # print(jose_li)
-    new_k += k-1  # 값 하나를 제거한 후이니 k-1만큼 더해줘야함
-
-# 출력 주의
-print('<',end='')
-for i in range(len(jose_li)):
-    if i != (len(jose_li)-1):
-        print(jose_li[i],end=', ')
-    else:
-        print('{}>'.format(jose_li[i]))
+print('<' + new + '>', sep = '')
