6f26dce94e93a8ad637f464fb4a9a0fdd9ddb610
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-01-31 08:25:18 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 121908 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,12 +1,16 @@
+# 2164_카드2_Card2
+
+
 from collections import deque
 
-T = int(input())
+n = int(input())
+dq = deque()
 
-deque = deque([i for i in range(1, T+1)])
+for i in range(1, n+1):
+    dq.append(i)
 
-while (len(deque) > 1):
-    deque.popleft()
-    tmp = deque.popleft()
-    deque.append(tmp)
-    
-print(deque[0])
+while len(dq) != 1:
+    dq.popleft()  # 가장 위의 카드를 제거
+    dq.append(dq.popleft())  # 가장 위의 카드를 가장 아래로 이동
+
+print(dq[0])
