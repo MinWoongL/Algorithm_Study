ad05d041718520d2b480d883f064b78c0b4a289a
MinWoongL <dlalsdnd2@gmail.com>
2023-01-31 17:29:10 +0900

    hanoi and statistics 수정

---
+++ Baekjoon/silver_to_gold/11729_hanoi.py
@@ -0,0 +1,23 @@
+# 11729_하노이 탑 이동순서_Hanoi
+
+def hanoi(n, li):
+    if n == 1:
+        return li.append((1,3))
+    elif n%2 == 1:
+        pass
+
+
+def cnt(n):
+    if n == 1:
+        return 1
+    else:
+        return cnt(n - 1) + 2**(n-1)
+
+
+N = int(input())
+
+move_li = []
+
+count = cnt(N)
+hanoi(N, move_li)
+print(count)
--- Baekjoon/silver_to_gold/2108_statistics.py
+++ Baekjoon/silver_to_gold/2108_statistics.py
@@ -5,30 +5,34 @@ import sys
 import operator
 
 
+# 산술평균 함수
 def arithmetic(li):
     return round(sum(li)/len(li))
 
 
+# 중앙값을 구하는 함수 -> N이 홀수로 고정이어서 2로나눈 몫으로 고정
 def median(li):
     li.sort()
     return li[len(li)//2]
 
 
+# 최빈값 함수 (Counter 사용하지 않음)
 def mode(li):
     if len(li) == 1:
         return li[0]
     else:
-        ndi = {}
+        ndi = {}  # list안의 수를 딕셔너리를 이용하여 count를 셈
         for v in li:
             if v not in ndi.keys():
                 ndi[v] = 1
             else:
                 ndi[v] += 1
         # print(ndi)
-        ndi = sorted(ndi.items(), key=operator.itemgetter(1))
+        ndi = sorted(ndi.items(), key=operator.itemgetter(1))  # value를 기준으로 정렬
         # print(ndi)
         # print(len(ndi))
-        idx = -1
+        idx = -1  # 최초인덱스 설정
+        # dict의 value가 큰 순서대로 최대값이 여러개면 계속 탐색 아니면 그 값 출력
         if ndi[idx][1] == ndi[idx - 1][1]:
             while ndi[idx][1] == ndi[idx - 1][1] and abs(idx) != (len(ndi) - 1):
                 idx -= 1
@@ -40,6 +44,7 @@ def mode(li):
             return ndi[idx][0]
 
 
+# 수의 범위를 구하는 함수
 def num_range(li):
     ma_value = max(li)
     mi_value = min(li)
