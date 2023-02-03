450e78a7fbe82b68d0b48b591d810a95be7e278a
MinWoongL <dlalsdnd2@gmail.com>
2023-02-03 15:35:47 +0900

    hanoi script

--- Baekjoon/silver_to_gold2_230201/11729_hanoi.py
+++ Baekjoon/silver_to_gold2_230201/11729_hanoi.py
@@ -1,7 +1,10 @@
 # 11729_하노이 탑 이동순서_Hanoi
 
+# a 시작위치, c = 최종위치, b = 중간위치
+
 def hanoi(n, li, a, b, c):
-    sub_li = []
+    sub_li = []  # 재귀로 넣어주는 hanoi 함수에 빈 리스트를 넣어주기 위해 생성
+    # n = 1,2 일 때, 해당 이동순서를 리스트에 저장 후 반환
     if n == 1:
         li.append([a, c])
         return li
@@ -11,28 +14,29 @@ def hanoi(n, li, a, b, c):
         li.append([b, c])
         return li
 
+    # 3단계로 나눠서 n-1 번째에 진행된 하노이탑 이동순서를 ans_li 에 저장
+
     ans_li = []
-    ans = hanoi(n-1, li, a, c, b)
-    for v in ans:
+    ans = hanoi(n - 1, li, a, c, b)
+    for v in ans:  # n-1 크기의 탑을 중간위치(b)에 이동
         ans_li.append(v)
 
-    ans_li.append([a, c])
+    ans_li.append([a, c])  # n 번째 블럭을 최종위치(c)에 이동
 
-    ans2 = hanoi(n-1, sub_li, b, a, c)
-    for v1 in ans2:
+    ans2 = hanoi(n - 1, sub_li, b, a, c)
+    for v1 in ans2:  # n-1 크기의 탑을 중간위치(b)에서 최종위치(c)로 이동
         ans_li.append(v1)
 
     return ans_li
 
 
-def cnt(n):
+def cnt(n):  # 하노이탑의 이동순서를 계산하는 함수
     if n == 1:
         return 1
     else:
-        return cnt(n - 1) + 2**(n-1)
+        return cnt(n - 1) + 2 ** (n - 1)
 
 
-# 12 13 23, 13 12 32 13 21 23 13, 12 13 23 12 31 32 12 13 23 21 31 23 12 13 23
 N = int(input())
 
 move_li = []
