59cbb55d6856bb18d75b8edc3718245ef062dd17
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-02-06 08:04:00 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 556 ms, Memory: 168008 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,29 +1,30 @@
 # 2447_별찍기_Recursion-starpoint
 
 # 1. *을 찍는방식으로 재귀, 2. 완성된 matrix에서 *을 지우는 방식으로 재귀
-def three_star_point(num):
-    if num == 3:
-        return ['***', '* *', '***']
+# 2번째 방식
+def three_star_point2(mat, num_now, num_first):
+    if num_now == 1:
+        return mat
 
-    else:
-        star_li = three_star_point(num//3)
-        text = []
-
-        for star in star_li:
-            text.append(star*3)
-            # print(text)
-
-        for star in star_li:
-            text. append(star+' '*(num//3)+star)
-
-        for star in star_li:
-            text.append(star*3)
-
-        return text
+    star_mat = three_star_point2(mat, num_now//3, num_first)
+    for k in range(0, num_now//3):
+        for l in range(0, num_now//3):
+            for i in range(num_now//3, (num_first-num_now//3), num_now):
+                for j in range(num_now//3, (num_first-num_now//3), num_now):
+                    star_mat[i+k][j+l] = ' '
+    # for star in star_matrix:
+    #     print(*star)
+    # print('')
+    return star_mat
 
 
 n = int(input())
+star_matrix = [['*'for x in range(n)] for y in range(n)]
 
-ans = three_star_point(n)
-# print(ans)
-print('\n'.join(ans))
+
+ans = three_star_point2(star_matrix, n, n)
+
+for star in ans:
+    print(*star, sep='')
+
+
