f67f068fed1d437802b9b92c48c88035377728e9
MinWoongLee <dlalsdnd2@gmail.com>
2023-02-05 10:57:47 +0900

    835-E 수정

--- Codeforce/835_4/E.py
+++ Codeforce/835_4/E.py
@@ -1,6 +1,5 @@
 # E_Binary Inversions
 import sys
-import copy
 
 T = int(input())
 
@@ -13,42 +12,51 @@ for tc in range(T):
     if len(nli) == 1:
         print(0)
     else:
-        nli_one = copy.copy(nli)
-        nli_zero = copy.copy(nli)
-        nli.clear()
+        nli_one = [nli[i] for i in range(n)]
+        nli_zero = [nli[i] for i in range(n)]
+        # 가장 앞쪽의 0을 1로 바꿔주면 해당 경우를 nli_one에 저장해주고 break
         while cnt == 0:
-            for i in range(len(nli_one)):
+            for i in range(n):
                 if nli_one[i] == 0:
                     nli_one[i] = 1
                     cnt += 1
                     break
             break
+        # 가장 뒷쪽의 1을 0으로 바꿔주면 nli_zero에 저장해주고 break
         while cnt2 == 0:
-            for j in range(len(nli_zero) - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
                 if nli_zero[j] == 1:
                     nli_zero[j] = 0
                     cnt2 += 1
                     break
             break
 
-        ans1 = 0
-        ans2 = 0
-        for i in range(len(nli_one)):
+        ans1 = 0  # 0을 1로 바꿔준 경우 순서쌍의 수
+        ans2 = 0  # 1을 0으로 바꿔준 경우 순서쌍의 수
+        ans3 = 0  # 아무것도 바꾸지 않은경우 순서쌍의 수
+        stack = []
+        for i in range(n):  # 1을 스택에 쌓아주면서 0이 나올때마다 스택에 쌓인 1의 수만큼 ans1 ++
             if nli_one[i] == 1:
-                for j in range(i, len(nli_one)):
-                    if nli_one[j] == 0:
-                        ans1 += 1
+                stack.append(1)
+            else:
+                if stack:
+                    ans1 += len(stack)
+        stack.clear()
 
-        for k in range(len(nli_zero)):
+        for k in range(n):
             if nli_zero[k] == 1:
-                for l in range(k, len(nli_zero)):
-                    if nli_zero[l] == 0:
-                        ans2 += 1
+                stack.append(1)
+            else:
+                if stack:
+                    ans2 += len(stack)
+        stack.clear()
 
-        print(max(ans1, ans2))
+        for num in range(n):
+            if nli[num] == 1:
+                stack.append(1)
+            else:
+                if stack:
+                    ans3 += len(stack)
 
-
-
-
-
+        print(max(ans1, ans2, ans3))
 
