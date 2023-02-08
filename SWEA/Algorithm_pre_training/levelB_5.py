25ee282ef22d380cca19721a45f5b131ae0a7f4c
MinWoongL <dlalsdnd2@gmail.com>
2023-02-08 16:24:41 +0900

    sudoku, fibo3

--- SWEA/Algorithm_pre_training/levelB_5.py
+++ SWEA/Algorithm_pre_training/levelB_5.py
@@ -1,44 +1,35 @@
-# 스도쿠 검증
-import sys
+# 스도쿠 검증_서울2반_이민웅
+T = int(input())
 
-sys.stdin = open('input.txt', "r")
-Tc = int(input())
 
 def sudoku_check(li):
-    status = 0
+    status = 1
+    for i in range(9):
+        for j in range(9):
+            for k in range(9):
+                if j != k:
+                    if li[i][j] == li[i][k]:
+                        status = 0
     for i in range(9):
         for j in range(8):
-            if li[i][j] == li[i][8]:
-                status = 0
-            else:
-                status = 1
+            for k in range(9):
+                if j != k:
+                    if li[j][i] == li[k][i]:
+                        status = 0
     for i in range(9):
         for j in range(8):
-            if li[j][i] == li[8][i]:
-                status = 0
-            else:
-                status = 1
-    # for i in range(9):
-    #     for j in range(8):
-    #         if i % 3 == 0 and j % 3 == 0:
-    #             for k in range(i, i+3):
-    #                 for l in range(j, j+3):
-                #         if li[i][j] == li[k][]
-                #
-                #
-                # if i % 3 == 0 and j % 3 == 0:
-                #     square = [0] * 10
-                #     for r in range(i, i + 3):
-                #         for c in range(j, j + 3):
-                #             num = M[r][c]
-                #             if square[num]: return 0
-                #             square[num] = 1
-
+            if i % 3 == 0 and j % 3 == 0:
+                for k in range(i, i + 3):
+                    for l in range(j, j + 3):
+                        if k != i or l != j:
+                            if li[i][j] == li[k][l]:
+                                status = 0
 
     return status
 
-for i in range(Tc):
-    sudoku = [list(map(int, input().split()))for _ in range(9)]
-    print(sudoku)
+
+for i in range(T):
+    sudoku = [list(map(int, input().split())) for _ in range(9)]
+
     ans = sudoku_check(sudoku)
-    print(ans)
+    print("#{} {}".format(i + 1, ans))
