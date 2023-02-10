f9148ae0f89837f3f872473b59f5e79c1e59692e
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-02-10 12:05:09 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,48 @@
+# 15650_N과M2
+
+def comb(li, n):
+    result = []
+    if n > len(li):
+        return result
+
+    if n == 1:
+        for i in li:
+            result.append([i])
+    else:
+        for i in range(len(li) - n + 1):
+            for j in comb(li[i+1:], n-1):
+                result.append([li[i]] + j)
+
+    return result
+
+
+# 순열 재귀 함수
+def perm(li, n):
+    result = []
+    if n > len(li):
+        return result
+
+    if n == 1:
+        for i in li:
+            result.append([i])
+    else:
+        for i in range(len(li)):
+            ans = [i for i in li]
+            ans.remove(li[i])
+            for k in perm(ans, n-1):
+                result.append([li[i]]+k)
+
+    return result
+
+
+N, M = map(int, input().split())
+
+num_li = []
+
+for i in range(1, N+1):
+    num_li.append(i)
+
+ans = comb(num_li, M)
+for v in ans:
+    print(*v)
+
