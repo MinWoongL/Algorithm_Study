ad09612340888b9a8952b05ac103ceeaef18b883
MinWoongLee <dlalsdnd2@gmail.com>
2023-02-12 21:50:25 +0900

    달팽이2차 수정

--- SWEA/Algorithm/230207/snail.py
+++ SWEA/Algorithm/230207/snail.py
@@ -4,53 +4,40 @@ T = int(input())
 
 for tc in range(1, T + 1):
     N = int(input())
-    num = N*N
+    num = N * N
     li = [[0 for p in range(N)] for o in range(N)]
 
     idx = 1
     i, j = 0, 0
     D = 0
-    while idx != num+1:
-        checker = 0
 
+    while idx != num + 1:
         if D % 4 == 0:
-            while j<N and checker < N and li[i][j] == 0:
-                if li[i][j] == 0:
-                    li[i][j] = idx
-                    j += 1
-                    idx += 1
-                else:
-                    j += 1
+            while 0 <= j < N and li[i][j] == 0:
+                li[i][j] = idx
+                j += 1
+                idx += 1
             j -= 1
             i += 1
         elif D % 4 == 1:
-            while i<N and checker < N and li[i][j] == 0:
-                if li[i][j] == 0:
-                    li[i][j] = idx
-                    i += 1
-                    idx += 1
-                else:
-                    i += 1
+            while 0 <= i < N and li[i][j] == 0:
+                li[i][j] = idx
+                i += 1
+                idx += 1
             i -= 1
             j -= 1
         elif D % 4 == 2:
-            while j > -1 and checker < N and li[i][j] == 0:
-                if li[i][j] == 0:
-                    li[i][j] = idx
-                    j -= 1
-                    idx += 1
-                else:
-                    j -= 1
+            while 0 <= j < N and li[i][j] == 0:
+                li[i][j] = idx
+                j -= 1
+                idx += 1
             j += 1
             i -= 1
         else:
-            while i > -1 and checker < N and li[i][j] == 0:
-                if li[i][j] == 0:
-                    li[i][j] = idx
-                    i -= 1
-                    idx += 1
-                else:
-                    i -= 1
+            while 0 <= i < N and li[i][j] == 0:
+                li[i][j] = idx
+                i -= 1
+                idx += 1
             i += 1
             j += 1
         D += 1
