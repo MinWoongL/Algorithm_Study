7134554df2f838289d510ec0e05f6bb853f31e15
MinWoongLee <dlalsdnd2@gmail.com>
2023-02-12 19:39:47 +0900

    14889

---
+++ Baekjoon/silver_to_gold2_230201/14889_startandlink.py
@@ -0,0 +1,46 @@
+# 14889_스타트와링크_start and link
+
+def comb(li, n):
+    result = []
+    if n > len(li):
+        return result
+    if n == 1:
+        for i in li:
+            result.append([i])
+    else:
+        for i in range(len(li)-n+1):
+            for j in comb(li[i+1:],n-1):
+                result.append([li[i]] + j)
+
+    return result
+
+
+N = int(input())
+
+n_mat = [list(map(int, input().split())) for _ in range(N)]
+
+team_a = 0
+team_b = 0
+
+comb_li = [i for i in range(1, N+1)]
+comb_li = comb(comb_li, N//2)  # 팀이 나눠질수 있는 모든 조합을 구함
+# print(comb_li)
+idx = 0
+ans = 10000000
+while idx <= len(comb_li)//2:
+    for i in range(len(comb_li[idx])):
+        for j in range(i+1, len(comb_li[idx])):
+            team_a += n_mat[comb_li[idx][i]-1][comb_li[idx][j]-1]  # 조합의 첫번째와 마지막은 서로 다른 대칭되는 인덱스
+            team_a += n_mat[comb_li[idx][j]-1][comb_li[idx][i]-1]
+    for i in range(len(comb_li[idx])):
+        for j in range(i+1, len(comb_li[idx])):
+            team_b += n_mat[comb_li[-idx-1][i]-1][comb_li[-idx-1][j]-1]  # 가장 앞, 가장 뒤 를 한칸씩 당기면서 순서대로 비교
+            team_b += n_mat[comb_li[-idx-1][j]-1][comb_li[-idx-1][i]-1]
+
+    if abs(team_a - team_b) < ans:
+        ans = abs(team_a - team_b)
+    team_a = 0
+    team_b = 0
+    idx += 1
+print(ans)
+
