417da913a57c14d503d6c8fb9c1a81379b0ea6eb
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-02-22 10:55:48 +0900

    [Gold IV] Title: 타일 채우기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -0,0 +1,24 @@
+# 2133_타일채우기_filling-tile
+
+N = int(input())
+if N == 1:
+    print(0)
+elif N == 2:
+    print(3)
+else:
+    dp = [0] * (N + 1)
+    dp[2] = 3
+    dp[3] = 0
+    for i in range(4, N + 1):
+        if i % 2 == 1:
+            dp[i] = 0
+        else:
+            dp[i] = (dp[i - 2] * 3) + 2 + 2 * sum(dp[:i - 3])
+
+    print(dp[-1])
+
+
+
+
+
+
