3b019531da435f87999c64293a266b31a116cdec
LeeMinWoong <dlalsdnd2@gmail.com>
2023-03-14 20:35:41 +0900

    backjoon 2문제

---
+++ Baekjoon/gold_to_platinum_230311/10844_easy-stairs.py
@@ -0,0 +1,44 @@
+# 10844_쉬운계단수_easy-numofstairs
+import sys
+
+N = int(input())
+
+dp = [0]*(N+1)
+cnt_num = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+cnt_num2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+dp[1] = 9
+for i in range(2, N+1):
+    cnt = 0
+    if i % 2 == 0:
+        for j in range(10):
+            if j == 0 or j == 9:
+                cnt += cnt_num[j]
+            else:
+                cnt += 2*cnt_num[j]
+        dp[i] = cnt
+        for k in range(10):
+            if k == 0:
+                cnt_num2[1] += cnt_num[k]
+            elif k == 9:
+                cnt_num2[8] += cnt_num[k]
+            else:
+                cnt_num2[k+1] += cnt_num[k]
+                cnt_num2[k-1] += cnt_num[k]
+        cnt_num = [0]*10
+    else:
+        for j in range(10):
+            if j == 0 or j == 9:
+                cnt += cnt_num2[j]
+            else:
+                cnt += 2*cnt_num2[j]
+        dp[i] = cnt
+        for k in range(10):
+            if k == 0:
+                cnt_num[1] += cnt_num2[k]
+            elif k == 9:
+                cnt_num[8] += cnt_num2[k]
+            else:
+                cnt_num[k+1] += cnt_num2[k]
+                cnt_num[k-1] += cnt_num2[k]
+        cnt_num2 = [0] * 10
+print(dp[N] % 1000000000)
--- Baekjoon/gold_to_platinum_230311/11779_minimum-cost.py
+++ Baekjoon/gold_to_platinum_230311/11779_minimum-cost.py
@@ -16,9 +16,14 @@ def dijksta(s, e):
         if distance > dis[node]:
             continue
 
+        for v in graph[node]:
+            weight = distance + v[1]
+            if weight < dis[v[0]]:
+                dis[v[0]] = weight
+                visited[v[0]] = node
+                heapq.heappush(q, (weight, v[0]))
 
-
-
+    return dis[e]
 
 
 N = int(input())
@@ -26,10 +31,25 @@ M = int(input())
 
 graph = [[] for _ in range(N+1)]
 
-for _ in range(M+2):
+for _ in range(M):
     s, e, c = map(int, input().split())
     graph[s].append((e, c))
 
 S, E = map(int, input().split())
 
+visited = [0]*(N+1)
 
+ans = dijksta(S, E)
+print(ans)
+# print(visited)
+
+ans_li = [E]
+t = E
+while t != S:
+    t = visited[t]
+    ans_li.append(t)
+
+print(len(ans_li))
+ans_li = ans_li[::-1]
+# print(ans_li)
+print(*ans_li, sep=' ')
