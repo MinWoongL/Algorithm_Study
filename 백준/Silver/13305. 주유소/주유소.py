ced2589c3d07828427514ab73ab689f13a14c1a7
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-03-16 19:38:40 +0900

    [Silver III] Title: 주유소, Time: 260 ms, Memory: 141620 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,15 @@
+# 13305_주유소_gas-station
+import sys
+from collections import deque
+N = int(input())
+dis_li = list(map(int, input().split()))
+cost_li = list(map(int, input().split()))
+
+cost = cost_li[0]
+ans = 0
+for i in range(len(dis_li)):
+    ans += dis_li[i]*cost
+    if cost_li[i+1] < cost:
+        cost = cost_li[i+1]
+
+print(ans)
