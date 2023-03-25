75cc9468e444160ba35ebea17ff1b485a14fe5a6
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-03-25 18:49:55 +0900

    [Silver I] Title: 신입 사원, Time: 2516 ms, Memory: 222064 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,21 +1,31 @@
-# 1946_신입사원_New-recruit
 import sys
-input = sys.stdin.readline
 
-T = int(input())
+T = int(sys.stdin.readline())
 
-for tc in range(T):
+for _ in range(T):
     N = int(input())
+    # lst = [list(map(int, input().split())) for _ in range(N)]
+    # for _ in range(N):
+    #     lst.append(list(map(int, input().split())))
+    lst = [0]*N
+    for _ in range(N):
+        a, b = map(int, input().split())
+        lst[a-1] = [a, b]
 
-    people = [list(map(int, input().split())) for _ in range(N)]
+    # 첫번째 점수로 정렬
+    # lst.sort()
+    li = [lst[0]]
 
-    people.sort(key=lambda x: x[0])
+    i = 0
+    j = 1
+    while True:
+        if j == N or lst[i][1] == 1:
+            break
+        if lst[i][1] < lst[j][1]:
+            j += 1
+        else:
+            li.append(lst[j])
+            i = j
+            j += 1
+    print(len(li))
 
-    cnt = 1
-    score = people[0][1]
-    for i in range(1, N):
-        if people[i][1] < score:
-            cnt += 1
-            score = people[i][1]
-
-    print(cnt)
