a8935e0f05a3fe24c40c9fd837cd6a878c919ec2
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-03-31 11:59:16 +0900

    [Silver II] Title: 부분수열의 합, Time: 344 ms, Memory: 115136 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+lst = list(map(int, input().split()))
+cnt = 0
+for i in range(1, 1 << N):
+    s = 0
+    for j in range(N):
+        if i & (1 << j):
+            s += lst[j]
+
+    if s == K:
+        cnt += 1
+
+print(cnt)
