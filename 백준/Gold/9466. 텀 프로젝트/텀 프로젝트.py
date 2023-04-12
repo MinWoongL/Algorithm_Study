373f3275edf36b3c810688eee3a8a11e6c39f6ad
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2023-04-12 22:35:50 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1548 ms, Memory: 253724 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10000)
+T = int(input())
+
+def dfs(node, visited, cycle):
+    visited[node] = True
+    cycle.append(node)
+    while True:
+        node = cycle[-1]
+        next_node = choice[node]-1
+        if visited[next_node]:
+            if next_node in cycle:
+                return cycle[cycle.index(next_node):]
+            else:
+                return []
+        else:
+            visited[next_node] = True
+            cycle.append(next_node)
+
+for tc in range(T):
+    N = int(input())
+    choice = list(map(int, input().split()))
+
+    team = [0]*(N+1)
+    visited = [False]*(N+1)
+
+    for i in range(N):
+        if not visited[i]:
+            cycle = []
+            cycle = dfs(i, visited, cycle)
+
+            for node in cycle:
+                team[node] = 1
+
+    print(N-sum(team))
