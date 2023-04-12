2aa09b19f7f9d750c244e1c48548c27e8bd2fdef
MinWoongLee <dlalsdnd2@gmail.com>
2023-04-12 22:36:53 +0900

    9466

--- Baekjoon/gold_to_platinum_230311/9466_term-project.py
+++ Baekjoon/gold_to_platinum_230311/9466_term-project.py
@@ -1,30 +1,38 @@
 # 9466_텀프로젝트-term-project
-# 시간초과
 import sys
 input = sys.stdin.readline
-
 T = int(input())
+
+def dfs(node, visited, cycle):
+    visited[node] = True
+    cycle.append(node)
+    while True:
+        node = cycle[-1]
+        next_node = choice[node]-1
+        if visited[next_node]:
+            if next_node in cycle:
+                return cycle[cycle.index(next_node):]
+            else:
+                return []
+        else:
+            visited[next_node] = True
+            cycle.append(next_node)
 
 for tc in range(T):
     N = int(input())
     choice = list(map(int, input().split()))
 
-    team = 0
-    v = [0]*(N+1)
+    team = [0]*(N+1)
+    visited = [False]*(N+1)
+
     for i in range(N):
-        check = [0]*(N+1)
-        cnt = 0
-        if v[i] == 0:
-            s = [i]
-            while s:
-                node = s.pop()
-                if not check[choice[node]-1]:
-                    s.append(choice[node]-1)
-                    check[choice[node]-1] = 1
-                    cnt += 1
-            if check[i] == 1:
-                team += cnt
-                for j in range(N+1):
-                    v[j] = check[j]
-    print(N-team)
+        if not visited[i]:
+            cycle = []
+            cycle = dfs(i, visited, cycle)
+
+            for node in cycle:
+                team[node] = 1
+
+    print(N-sum(team))
+
 
