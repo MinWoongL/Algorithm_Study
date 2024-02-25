8a35bd2433ae41b836e5cdbc9d0ff7efe621e7ee
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-02-25 23:02:57 +0900

    [Gold IV] Title: LCS 2, Time: 576 ms, Memory: 427616 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -2,27 +2,31 @@
 import sys
 input = sys.stdin.readline
 
+def find_lcs(w1, w2):
+    len_w1 = len(w1)
+    len_w2 = len(w2)
+    dp = [["" for _ in range(len_w2 + 1)] for _ in range(len_w1 + 1)]
+
+    for i in range(1, len_w1 + 1):
+        for j in range(1, len_w2 + 1):
+            if w1[i - 1] == w2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + w1[i - 1]
+            else:
+                if len(dp[i - 1][j]) >= len(dp[i][j - 1]):
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i][j - 1]
+
+    return dp[len_w1][len_w2]
+
+
 w1 = list(input().strip())
 w2 = list(input().strip())
-len_w1 = len(w1)
-len_w2 = len(w2)
-dp = [[[0, ''] for _ in range(len_w2+1)] for _ in range(len_w1+1)]
+ans = find_lcs(w1, w2)
 
-
-for i in range(1, len_w1+1):
-    for j in range(1, len_w2+1):
-        if w1[i-1] == w2[j-1]:
-            dp[i][j] = [dp[i-1][j-1][0]+1, dp[i-1][j-1][1] + w1[i-1]]
-        else:
-            if dp[i][j-1][0] >= dp[i-1][j][0]:
-                dp[i][j] = dp[i][j-1]
-            else:
-                dp[i][j] = dp[i-1][j]
-
-
-if dp[-1][-1][0] == 0:
+if len(ans) == 0:
     print(0)
 else:
-    print(dp[-1][-1][0])
-    print(dp[-1][-1][1])
+    print(len(ans))
+    print(ans)
 
