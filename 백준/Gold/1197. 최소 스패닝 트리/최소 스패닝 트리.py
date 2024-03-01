c4637e222caa0d1e0cf04c3d02fa36a1e1adc9b1
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-03-01 23:29:42 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 336 ms, Memory: 123776 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,39 +1,43 @@
-# 1197_최소 스패닝 트리_MST
 import sys
 input = sys.stdin.readline
 
-
 def findset(node):
-    while parent[node] != node:
-        node = parent[node]
-    return node
-
+    # 경로 압축 기법을 적용하여 최상위 부모를 찾고, 경로상의 모든 노드를 직접 최상위 부모에 연결
+    if parent[node] != node:
+        parent[node] = findset(parent[node])
+    return parent[node]
 
 def union(x, y):
-    parent[findset(y)] = findset(x)
-
+    rootX = findset(x)
+    rootY = findset(y)
+    
+    # 랭크 기반 병합을 적용하여 트리의 높이를 최소화
+    if rank[rootX] < rank[rootY]:
+        parent[rootX] = rootY
+    else:
+        parent[rootY] = rootX
+        if rank[rootX] == rank[rootY]:
+            rank[rootX] += 1
 
 V, E = map(int, input().split())
 
 graph = []
-parent = [i for i in range(V+1)]
+parent = [i for i in range(V+1)]  # 부모 노드 초기화
+rank = [0] * (V + 1)  # 랭크(트리의 높이)를 저장할 배열 초기화
+
 for _ in range(E):
     s, g, w = map(int, input().split())
     graph.append([s, g, w])
 
-graph.sort(key=lambda x: x[2])
+graph.sort(key=lambda x: x[2])  # 간선을 가중치에 따라 정렬
 
-weight = 0
-cnt = 0
+weight = 0  # 최소 스패닝 트리의 가중치 합계
 
 for e in graph:
     u, v, w = e
-    if cnt == V:
-        break
-
+    # 사이클이 발생하지 않는 경우에만 간선을 선택
     if findset(u) != findset(v):
-        cnt += 1
-        weight += w
         union(u, v)
+        weight += w
 
 print(weight)
