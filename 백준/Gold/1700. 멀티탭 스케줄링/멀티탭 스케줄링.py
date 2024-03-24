6f87f0a968645427059d0d3814d8e313a5a2a11f
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-03-25 02:16:01 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -9,24 +9,38 @@ using_app = set()
 cnt = 0
 ans = 0
 
+next_use = {}
+
+for i in range(K):
+    if appliances[i] not in next_use.keys():
+        next_use[appliances[i]] = []
+    else:
+        next_use[appliances[i]].append(i)
+
+
 for i in range(K):
     tmp = appliances[i]
     if appliances[i] in using_app:
+        next_use[tmp].pop(0)
         continue
     else:
         if cnt != N:
             using_app.add(tmp)
             cnt += 1
         else:
-            tmp_set = set(k for k in using_app)
-            for j in range(i+1, K):
-                if len(tmp_set) == 1:
+            change = [-1, -1]
+            for s in using_app:
+                if not next_use[s]:
+                    using_app.remove(s)
+                    using_app.add(tmp)
                     break
-                if appliances[j] in tmp_set:
-                    tmp_set.remove(appliances[j])
-            change = tmp_set.pop()
-            using_app.remove(change)
-            using_app.add(tmp)
+                else:
+                    if next_use[s][0] > change[1]:
+                        change = [s, next_use[s][0]]
+            else:
+                next_use[change[0]].pop(0)
+                using_app.remove(change[0])
+                using_app.add(tmp)
             ans += 1
 
 print(ans)
