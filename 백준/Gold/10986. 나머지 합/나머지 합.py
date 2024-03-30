6064a86afe309ab0d1955191d572e3cea41a3c52
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-03-31 01:28:56 +0900

    [Gold III] Title: 나머지 합, Time: 928 ms, Memory: 142240 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -13,13 +13,14 @@ rest_dict = {0: 1}
 for i in range(N):
     tmp = (prefix[-1] + n_lst[i])%M
     if tmp in rest_dict.keys():
+        cnt += rest_dict[tmp]
         rest_dict[tmp] += 1
     else:
         rest_dict[tmp] = 1
     prefix.append(tmp)
 
-for i in range(N+1):
-    rest_dict[prefix[i]] -= 1
-    cnt += rest_dict[prefix[i]]
+# for i in range(N+1):
+#     rest_dict[prefix[i]] -= 1
+#     cnt += rest_dict[prefix[i]]
 
 print(cnt)
