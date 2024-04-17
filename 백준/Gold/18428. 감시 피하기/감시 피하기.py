40c1ca94520ca556ee368e4338922545165ef65a
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-04-17 23:11:11 +0900

    [Gold V] Title: 감시 피하기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,81 +1,65 @@
+# 18428_감시피하기_avoid surveilance
 import sys
-
 input = sys.stdin.readline
-from itertools import combinations
+dxy = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-N = int(input())
-arr = [list(map(str, input().strip().split())) for _ in range(N)]
-
-stu = []
-tea = []
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == 'S':
-            stu.append((i, j))
-        elif arr[i][j] == 'T':
-            tea.append((i, j))
-
-
-def ifBlocked(arr):
-    for ti, tj in tea:
-        n = 1
-        while n < N:
-            if 0 <= ti - n < N:
-                if arr[ti - n][tj] == 'O':
+def teacher_check(field):
+    # visited = [[0]*N for _ in range(N)]
+    for t in teachers:
+        for d in dxy:
+            x = t[0]
+            y = t[1]
+            while True:
+                x = x + d[0]
+                y = y + d[1]
+                if 0 <= x <= N-1 and 0 <= y <= N-1:
+                    if field[x][y] == 'S':
+                        return False
+                    elif field[x][y] == "O" or field[x][y] == "T":
+                        break
+                else:
                     break
-                elif arr[ti - n][tj] == 'S':
-                    return False
-            else:
-                break
-            n += 1
-        n = 1
-        while n < N:
-            if 0 <= ti + n < N:
-                if arr[ti + n][tj] == 'O':
-                    break
-                elif arr[ti + n][tj] == 'S':
-                    return False
-            else:
-                break
-            n += 1
-        n = 1
-        while n < N:
-            if 0 <= tj - n < N:
-                if arr[ti][tj - n] == 'O':
-                    break
-                elif arr[ti][tj - n] == 'S':
-                    return False
-            else:
-                break
-            n += 1
-        n = 1
-        while n < N:
-            if 0 <= tj + n < N:
-                if arr[ti][tj + n] == 'O':
-                    break
-                elif arr[ti][tj + n] == 'S':
-                    return False
-            else:
-                break
-            n += 1
     return True
 
 
-Xlst = []
+def bt(field, x, y, obs_cnt):
+    global ans
+    if ans == "YES":
+        return
+
+    if y == N-1:
+        y = 0
+        x += 1
+    else:
+        y += 1
+
+    if x == N and y == 0:
+        return
+
+    if obs_cnt == 3:
+        tmp = teacher_check(field)
+        if tmp:
+            ans = 'YES'
+        return
+
+    if field[x][y] == "X":
+        field[x][y] = 'O'
+        bt(field, x, y, obs_cnt+1)
+        field[x][y] = "X"
+        bt(field, x, y, obs_cnt)
+    else:
+        bt(field, x, y, obs_cnt)
+
+
+N = int(input())
+
+field = [list(input().split()) for _ in range(N)]
+ans = "NO"
+teachers = []
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == 'X':
-            Xlst.append((i, j))
+        if field[i][j] == 'T':
+            teachers.append([i, j])
 
-for i in combinations(Xlst, 3):
-    for x, y in i:
-        arr[x][y] = 'O'
-
-    if ifBlocked(arr):
-        print('YES')
-        break
-    else:
-        for x, y in i:
-            arr[x][y] = 'X'
-else:
-    print('NO')
+bt(field, 0, -1, 0)
+print(ans)
