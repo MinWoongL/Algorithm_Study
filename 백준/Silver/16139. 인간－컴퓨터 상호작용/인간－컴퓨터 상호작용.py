e3a3021afee571b10216e7473c9ed24aef1cd095
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-04-21 23:38:02 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 44 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -2,18 +2,11 @@
 import sys
 input = sys.stdin.readline
 
-S = input().strip()
+S = input()
 Q = int(input())
-
-sum_value = [[0 for _ in range(26)] for _ in range(len(S)+1)]
-
-for i in range(1, len(S)+1):
-    for j in range(26):
-        sum_value[i][j] = sum_value[i-1][j]
-    sum_value[i][ord(S[i-1])-97] += 1
 
 for _ in range(Q):
     alpha, s, e = input().split()
     s, e = int(s), int(e)
-
-    print(sum_value[e+1][ord(alpha)-97] - sum_value[s][ord(alpha)-97])
+    # print(alpha, s, e)
+    print(S[s:e+1].count(alpha))
