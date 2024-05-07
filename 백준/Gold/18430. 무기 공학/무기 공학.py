c18f66c1d6bbb06fe95dc7a990daf17556766d2c
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-05-07 16:52:26 +0900

    [Gold IV] Title: 무기 공학, Time: 872 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/18430. 무기 공학/무기 공학.py
@@ -0,0 +1,52 @@
+# 18430_무기공학
+import sys
+input = sys.stdin.readline
+dxy = [[(1, 0), (0, -1)], [(0, -1), (-1, 0)], [(-1, 0), (0, 1)], [(1, 0), (0, 1)]]
+
+
+def bt(x, y, v, score):
+    global ans
+    y += 1
+    if y == M:
+        x = x+1
+        y = 0
+
+    if x == N and y == 0:
+        if score > ans:
+            ans = score
+        return
+
+    if not v[x][y]:
+        for ds in dxy:
+            tmp = []
+            tmp_cordi = []
+            for d in ds:
+                nx = x + d[0]
+                ny = y + d[1]
+                if 0 <= nx <= N-1 and 0 <= ny <= M-1 and not v[nx][ny]:
+                    tmp.append(woods[nx][ny])
+                    tmp_cordi.append([nx, ny])
+                else:
+                    break
+            else:
+                tmp_score = sum(tmp) + 2*woods[x][y]
+                for c in tmp_cordi:
+                    v[c[0]][c[1]] = 1
+                v[x][y] = 1
+                bt(x, y, v, tmp_score+score)
+                v[x][y] = 0
+                for c in tmp_cordi:
+                    v[c[0]][c[1]] = 0
+    bt(x, y, v, score)
+
+
+
+N, M = map(int, input().split())
+woods = [list(map(int, input().split())) for _ in range(N)]
+
+visited = [[0]*M for _ in range(N)]
+ans = 0
+
+bt(0, -1, visited, 0)
+
+print(ans)
