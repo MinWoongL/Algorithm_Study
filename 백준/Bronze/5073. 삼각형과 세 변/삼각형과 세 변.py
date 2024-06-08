464adc8c8a4fcfd9fbc6bc4eba6553094203f983
MinWoongL <65647667+MinWoongL@users.noreply.github.com>
2024-06-09 00:51:38 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a, b, c = map(int, input().split())
+    if a == 0 and b == 0 and c == 0:
+        break
+
+    if max(a, b, c) >= (a+b+c) - max(a, b, c):
+        print("Invalid")
+        continue
+
+    if a == b and b == c:
+        print('Equilateral')
+    elif a == b or b == c or c == a:
+        print("Isosceles")
+    else:
+        print('Scalene')
